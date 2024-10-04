f8b3e94677c89a75482a7908ce532e81157a3066
aurora-0025 <ritingeorge.d@gmail.com>
2024-10-05 02:50:52 +0530

    fix: textoverflow in name and protocol

--- src/windows/Search.tsx
+++ src/windows/Search.tsx
@@ -47,21 +47,21 @@ function Search() {
         autoFocus
       />
       {results.length > 0 && (
-        <div className="mt-2 max-h-[200px] min-w-[600px] overflow-y-auto border-t border-gray-300">
+        <div className="mt-2 max-h-[200px] min-w-[600px] overflow-y-auto border-t border-gray-300 overflow-x-hidden">
           <ul>
             {results.map((result, index) => (
               <li
                 key={index}
-                className={`flex w-full items-center ${index != results.length - 1 && "border-b border-gray-300"} p-2`}
+                className={`flex items-center ${index != results.length - 1 && "border-b border-gray-300"} p-2`}
               >
-                <div className="flex items-center gap-2">
+                <div className="flex items-center gap-2 w-full">
                   <img
                     className="h-10 w-10"
-                    src={`clipse:\\\\${result.path}`}
+                    src={`clipse://${result.path}`}
                   />
-                  <div className="flex flex-col justify-center">
-                    <b>{result.name}</b>
-                    <p className="mb-1 text-sm">{result.path}</p>
+                  <div className="flex w-[80%] flex-col justify-center text-ellipsis">
+                    <b className="overflow-hidden whitespace-nowrap overflow-ellipsis">{result.name}</b>
+                    <p className="mb-1 text-sm overflow-hidden whitespace-nowrap overflow-ellipsis">{result.path}</p>
                   </div>
                 </div>
               </li>
